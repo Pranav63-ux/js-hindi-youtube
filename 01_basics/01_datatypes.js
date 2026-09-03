3c6dece9436762055044e68dfa7079e3a79a5af7
Pranav63-ux <pranavdharsandia493@gmail.com>
2026-09-03 07:07:07 +0000

    datatype and variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,16 @@
+//  number datatypes => range 2 ki power 53
+// bigint => it is used to store large value
+// boolean datatype => true/false
+// string datatype => "  "  (anything which is written in this doble quotes)
+// null => standalone value
+// undefined datatype => means undefined
+// symbol datatype => unique
+
+
+
+let age = 67
+let name = "pranav"
+console.log(typeof age);
+console.log(typeof name);
+export{};
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "pranav@gmail.com"
+let accountState
+
+
+accountEmail = "pranav273@gamil.com"
+
+
+console.table([accountId,accountEmail,accountState]);
+
+
+// const => is used for constant which is nit change during the program execution.
+// let => is used for like it is changed during the during the program.
+ 
+
